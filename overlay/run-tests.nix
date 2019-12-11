54bf1d9093e74cee3e11fe1912b01697fecf5a82
Trinh Hoang Anh <anh@tenx.tech>
2019-12-11 15:06:11 +0800

    Create a proper derivation

--- overlay/run-tests.nix
+++ overlay/run-tests.nix
@@ -1,16 +1,16 @@
 # Run all tests for a crate.
-{ runCommand }:
+{ stdenvNoCC }:
 crate: env:
 let
   testBins = crate { compileMode = "test"; };
 in
-  runCommand
-    "test-${testBins.name}"
-    ({
-      inherit (testBins) src;
-      CARGO_MANIFEST_DIR = testBins.src;
-    } // env)
-    ''
+  stdenvNoCC.mkDerivation (env // {
+    name = "test-${testBins.name}";
+    inherit (testBins) src;
+    CARGO_MANIFEST_DIR = testBins.src;
+    phases = [ "unpackPhase" "checkPhase" ];
+    doCheck = true;
+    checkPhase = ''
         for f in ${testBins}/bin/*; do
           # HACK: cargo produces the crate's main binary in the bin directory if the crate contains example tests.
           # The `grep` filters out the main binary, which doesn't contain the help string found in test binaries.
@@ -19,4 +19,5 @@ in
           fi
         done
         touch $out
-    ''
+    '';
+  })
