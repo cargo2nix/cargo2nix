ee1694cfdd2b830c7f53d0082fe9f3ed27c6176c
Trinh Hoang Anh <anh@tenx.tech>
2019-12-11 15:24:31 +0800

    Allow specifying testCommand

--- overlay/run-tests.nix
+++ overlay/run-tests.nix
@@ -1,23 +1,26 @@
 # Run all tests for a crate.
 { stdenvNoCC }:
-crate: env:
+crate:
+env@{
+  testCommand ? bin: "${bin}",
+}:
 let
   testBins = crate { compileMode = "test"; };
 in
-  stdenvNoCC.mkDerivation (env // {
+  stdenvNoCC.mkDerivation ((removeAttrs env [ "testCommand" ]) // {
     name = "test-${testBins.name}";
     inherit (testBins) src;
     CARGO_MANIFEST_DIR = testBins.src;
     phases = [ "unpackPhase" "checkPhase" ];
     doCheck = true;
     checkPhase = ''
-        for f in ${testBins}/bin/*; do
-          # HACK: cargo produces the crate's main binary in the bin directory if the crate contains example tests.
-          # The `grep` filters out the main binary, which doesn't contain the help string found in test binaries.
-          if [[ -x "$f" ]] && grep "By default, all tests are run in parallel" "$f"; then
-            $f
-          fi
-        done
-        touch $out
+      for f in ${testBins}/bin/*; do
+        # HACK: cargo produces the crate's main binary in the bin directory if the crate contains example tests.
+        # The `grep` filters out the main binary, which doesn't contain the help string found in test binaries.
+        if [[ -x "$f" ]] && grep "By default, all tests are run in parallel" "$f"; then
+          ${testCommand "$f"}
+        fi
+      done
+      touch $out
     '';
   })
