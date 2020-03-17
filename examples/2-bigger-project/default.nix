ca8161561abe2abb0bb3ef3db4f24036028d9978
Eyal Kalderon <eyal.kalderon@tenx.tech>
2020-03-17 15:17:04 +0800

    Restructure second example project
    As with the hello-world project, we take a more hands-on and less
    intimidating approach to teaching `cargo2nix` by building a simple HTTP
    client with `reqwest` and gently introducing the concept of overrides.
    
    The next example will show how to Nix-ify a small crate workspace and
    build and run some simple tests.

---
+++ examples/2-bigger-project/default.nix
@@ -0,0 +1,27 @@
+{
+  system ? builtins.currentSystem,
+  nixpkgsMozilla ? builtins.fetchGit {
+    url = https://github.com/mozilla/nixpkgs-mozilla;
+    rev = "50bae918794d3c283aeb335b209efd71e75e3954";
+  },
+  cargo2nix ? builtins.fetchGit {
+    url = https://github.com/tenx-tech/cargo2nix;
+    ref = "v0.8.0";
+  },
+}:
+let
+  rustOverlay = import "${nixpkgsMozilla}/rust-overlay.nix";
+  cargo2nixOverlay = import "${cargo2nix}/overlay";
+
+  pkgs = import <nixpkgs> {
+    inherit system;
+    overlays = [ cargo2nixOverlay rustOverlay ];
+  };
+
+  rustPkgs = pkgs.rustBuilder.makePackageSet' {
+    rustChannel = "stable";
+    packageFun = import ./Cargo.nix;
+    packageOverrides = pkgs: pkgs.rustBuilder.overrides.all;
+  };
+in
+  rustPkgs.workspace.bigger-project {}
