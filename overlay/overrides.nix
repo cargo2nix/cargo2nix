82d3c1ac1f3946f73681d08af9a37b88a68d473e
Psionik K <73710933+psionic-k@users.noreply.github.com>
2021-01-12 13:34:27 +0800

    accomodate openssl-sys build.rs
    It was attempting to do something it could not

--- overlay/overrides.nix
+++ overlay/overrides.nix
@@ -169,6 +169,7 @@ in rec {
           { name = "RUSTFLAGS"; value = "--cfg ossl111 --cfg ossl110 --cfg ossl101";}
           { name = "${envize pkgs.stdenv.buildPlatform.config}_OPENSSL_DIR"; value = joinOpenssl (patchOpenssl pkgs.buildPackages); }
           { name = "${envize pkgs.stdenv.hostPlatform.config}_OPENSSL_DIR"; value = joinOpenssl (patchOpenssl pkgs); }
+          { name = "OPENSSL_NO_VENDOR"; value = "1";} # fixed 0.9.60
         ])
       ];
     };
