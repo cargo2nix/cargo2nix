3d844b28f2f3966aad267c280bbc2a5cba8b3417
Andrey Kuznetsov <fear@loathing.in>
2021-02-06 11:36:31 +0000

    simple flakes support

---
+++ examples-flakes/1-hello-world/flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs-mozilla = { url = github:mozilla/nixpkgs-mozilla; flake = false; };
+    cargo2nix.url = github:onsails/cargo2nix/flake;
+    utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { nixpkgs, utils, cargo2nix, nixpkgs-mozilla, ... }: utils.lib.eachDefaultSystem (system:
+    let
+      rustChannel = "1.49.0";
+      rustChannelSha256 = "KCh2UBGtdlBJ/4UOqZlxUtcyefv7MH1neoVNV4z0nWs=";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (import "${nixpkgs-mozilla}/rust-overlay.nix")
+          cargo2nix.overlay
+          (
+            self: super: {
+              rustStable = (
+                super.rustChannelOf {
+                  channel = rustChannel;
+                  sha256 = rustChannelSha256;
+                }
+              ).rust;
+            }
+          )
+        ];
+      };
+      rustPkgs = pkgs.rustBuilder.makePackageSet' {
+        inherit rustChannel rustChannelSha256;
+        packageFun = import ./Cargo.nix;
+      };
+    in
+    {
+      defaultPackage = rustPkgs.workspace.hello-world { };
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustStable
+        ];
+      };
+    }
+  );
+}
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    utils.url = github:numtide/flake-utils;
+    nixpkgs-mozilla = { url = github:mozilla/nixpkgs-mozilla; flake = false; };
+  };
+
+  outputs = { self, nixpkgs, utils, nixpkgs-mozilla, ... }@inputs:
+    let cargo2nixOverlay = import ./overlay;
+    in
+    {
+      overlay = cargo2nixOverlay;
+    } // utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            cargo2nixOverlay
+            (import "${nixpkgs-mozilla}/rust-overlay.nix")
+          ];
+        };
+        rustChannel = "1.49.0";
+        rustChannelSha256 = "KCh2UBGtdlBJ/4UOqZlxUtcyefv7MH1neoVNV4z0nWs=";
+        rustPkgs =
+          pkgs.rustBuilder.makePackageSet' {
+            packageFun = import ./Cargo.nix;
+            inherit rustChannel rustChannelSha256;
+            packageOverrides = pkgs: pkgs.rustBuilder.overrides.all;
+            localPatterns = [ ''^(src|tests|templates)(/.*)?'' ''[^/]*\.(rs|toml)$'' ];
+          };
+      in
+      {
+        defaultPackage = rustPkgs.workspace.cargo2nix { };
+        defaultApp = {
+          type = "app";
+          program = "${self.defaultPackage.${system}}/bin/cargo2nix";
+        };
+      });
+}
