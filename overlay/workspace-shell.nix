ff07c97cddbe84e763c20e57caa1f90f815d5168
Psionik K <73710933+psionic-k@users.noreply.github.com>
2021-12-02 13:29:45 +0800

    Make shell pass through customizations

--- overlay/workspace-shell.nix
+++ overlay/workspace-shell.nix
@@ -1,11 +1,16 @@
 { pkgs, noBuild, rustChannel }:
-{}:
+args@{
+  inputsFrom ? [],
+  nativeBuildInputs ? [],
+  ...
+}:
 
-pkgs.mkShell {
+pkgs.mkShell (args // {
   # `noBuild` is a special crate set used to create a development shell
   # containing all native dependencies provided by the overrides above.
   # `cargo build` within the shell should just work.
-  inputsFrom = pkgs.lib.mapAttrsToList (_: pkg: pkg { }) noBuild.workspace;
-  nativeBuildInputs = [ rustChannel ] ++ (with pkgs; [cacert]);
+  inputsFrom = (pkgs.lib.mapAttrsToList (_: pkg: pkg { }) noBuild.workspace) ++ inputsFrom;
+  nativeBuildInputs = [ rustChannel ] ++ (with pkgs; [cacert]) ++ nativeBuildInputs;
+  # Configures tools like Rust Analyzer to locate the correct rust-src
   RUST_SRC_PATH = "${rustChannel}/lib/rustlib/src/rust/library";
-}
+})
