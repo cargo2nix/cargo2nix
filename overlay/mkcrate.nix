c9728e2ca3dc7c087d1b6f5473683764b6e2c68b
Psionik K <73710933+psionic-k@users.noreply.github.com>
2022-10-03 00:10:45 -0500

    support passing unstable cargo flags
    This only works with nightly toolchains.  Likely doesn't work at all whenever
    the flag should affect dependencies, which cargo cannot access / decide within
    the sandbox

--- overlay/mkcrate.nix
+++ overlay/mkcrate.nix
@@ -21,6 +21,7 @@
   profileOpts ? null,
   codegenOpts ? null,
   meta ? { },
+  cargoUnstableFlags ? [ ],
   rustcLinkFlags ? [ ],
   rustcBuildFlags ? [ ],
   target ? null,
@@ -82,6 +83,7 @@ let
       if compileMode != "doctest" then ''
         ${rustToolchain}/bin/cargo build $CARGO_VERBOSE ${optionalString release "--release"} --target ${rustHostTriple} ${buildMode} \
           ${featuresArg} ${optionalString (!hasDefaultFeature) "--no-default-features"} \
+          ${optionalString (builtins.length cargoUnstableFlags > 0) "-Z ${lib.strings.concatStringsSep "," cargoUnstableFlags}"} \
           --message-format json-diagnostic-rendered-ansi | tee .cargo-build-output \
           1> >(jq 'select(.message != null) .message.rendered' -r)
       ''
